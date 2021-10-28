bfcc569b9f151e1f25f996d5bc62bcfde3dbb634
alkmnd <asfsdg>
2021-10-28 19:00:29 +0300

    The project contains NavigationController and CollectionView with cell.

--- chpanPW4/ViewController.swift
+++ chpanPW4/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emptyCollectionLabel: UILabel!
+    @IBOutlet weak var notesCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
